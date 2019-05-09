609b8227cbb6abb6261eb5ae4b17053e67df714c
Capuno, Ma Rosario N <ma.rosario.n.capuno@accenture.com>
2019-05-10 00:13:32 +0800

    feat(first common med entry): implementation of reading from file
    renaming of file concatenating to produce med name
    
    BREAKING CHANGE: none
    Closes 0

---
+++ ClassLibrary1/ClassLibrary1/Combination/CommonMeds.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ClassLibrary1
+{
+    public class CommonMeds
+    {
+        public List<Type> GetTypes()
+        {
+            List<Type> commonTypes = new List<Type>();
+
+            commonTypes.AddRange(Assembly.GetExecutingAssembly()
+                    .GetTypes()
+                    .Where(t => t.IsEnum && t.Namespace == "Common"));
+
+            return commonTypes;
+        }
+
+        /// <summary>
+        /// Filename makes the enum grouped by its purpose
+        /// </summary>
+        /// <returns></returns>
+        private List<string> GetFileNames()
+        {
+            List<string> fileNames = new List<string>();
+            var currentFileName = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
+            DirectoryInfo directoryInfo = new DirectoryInfo(currentFileName);
+
+            foreach (var file in directoryInfo.Parent
+                                              .Parent.GetDirectories()
+                                              .Where(d => d.Name == "Enum")
+                                              .First()
+                                              .GetDirectories()
+                                              .Where(d => d.Name == "Common")
+                                              .First()
+                                              .GetFiles())
+            {
+                fileNames.Add(file.Name.Replace(".cs", ""));
+            }
+
+            return fileNames;
+        }
+
+        public List<string> Print()
+        {
+            List<string> commonMeds = new List<string>();
+            List<string> incomplete = new List<string>();
+
+            foreach(var purpose in GetFileNames())
+            {
+                var purposeTypes = GetTypes().Where(t => t.Name.Contains(purpose));
+                var creativeType = purposeTypes.Where(t => t.Name.Contains("Creative")).First().UnderlyingSystemType;
+                var substem = purposeTypes.Where(t => t.Name.Contains("Substem"));
+                var stemType = purposeTypes.Where(t => t.Name.Contains("Stem")).First().UnderlyingSystemType;
+
+                var creativeTypeValues = Enum.GetValues(creativeType);
+                foreach (var creative in creativeTypeValues)
+                {
+                    incomplete.Add(creative.ToString());
+                }
+
+                if (substem.FirstOrDefault() == null)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                incomplete.AddRange(
+                    MultiplyStem(creativeTypeValues.Length, stemType));
+
+                int back = 0;
+
+                for (int i = 0; i < incomplete.Count; i++)
+                {
+                    if (i < creativeTypeValues.Length)
+                    {
+                        commonMeds.Add("");
+                    }
+                    else if ((back + 1) % creativeTypeValues.Length != 0)
+                    {
+                        if (back < creativeTypeValues.Length) { }
+                        else { back = 0; }
+                    }
+                    commonMeds[back] += incomplete[i];
+                    back++;
+                }
+            }
+
+            return commonMeds;
+        }
+
+        private List<string> MultiplyStem(int creativeCount, Type type)
+        {
+            List<string> stems = new List<string>();
+            for (int i = 0; i < creativeCount; i++)
+            {
+                foreach (var monoclonalAntibodySubStem in Enum.GetValues(type))
+                {
+                    stems.Add(monoclonalAntibodySubStem.ToString());
+                }
+            }
+            return stems;
+        }
+    }
+}
