129ab1325af448ccf5438cc09c5ecec21896d238
Capuno, Ma Rosario N <ma.rosario.n.capuno@accenture.com>
2019-05-09 19:11:32 +0800

    combine stems and print

---
+++ ClassLibrary1/ClassLibrary1/Combination.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Combination
+    {
+        public List<string> PrintCancerTargetTherapy()
+        {
+            List<string> cancerTargetTherapies = new List<string>();
+            List<string> incomplete = new List<string>();
+
+            foreach (var monoclonalAntibodyCreative in Enum.GetValues(typeof(MonoclonalAntibodyCreative)))
+            {
+                incomplete.Add(monoclonalAntibodyCreative.ToString());
+            }
+
+            foreach (var monoclonalAntibodyTarget in Enum.GetValues(typeof(MonoclonalAntibodyTarget)))
+            {
+                incomplete.Add(monoclonalAntibodyTarget.ToString().Substring(0,2));
+            }
+
+            foreach (var monoclonalAntibodySubStem in Enum.GetValues(typeof(MonoclonalAntibodySubStem)))
+            {
+                incomplete.Add(monoclonalAntibodySubStem.ToString().Substring(0, 2));
+            }
+
+            incomplete.AddRange(
+                MultiplyStem(Enum.GetValues(typeof(MonoclonalAntibodyCreative)).Length, typeof(MonoclonalAntibodyStem)));
+
+            int back = 0;
+
+            for (int i = 0; i < incomplete.Count; i++)
+            {
+                if (i < Enum.GetValues(typeof(MonoclonalAntibodyCreative)).Length)
+                {
+                    cancerTargetTherapies.Add("");
+                }
+                else if ((back + 1) % Enum.GetValues(typeof(MonoclonalAntibodyCreative)).Length != 0)
+                {
+                    if (back < Enum.GetValues(typeof(MonoclonalAntibodyCreative)).Length) { }
+                    else { back = 0; }
+                }
+                cancerTargetTherapies[back] += incomplete[i];
+                back++;
+            }
+
+            return cancerTargetTherapies;
+        }
+
+        public List<string> PrintCancerInhibitorTherapy()
+        {
+            List<string> cancerInhibitors = new List<string>();
+            List<string> incomplete = new List<string>();
+
+            foreach (var smallMoleculeInhibitorCreative in Enum.GetValues(typeof(SmallMoleculeInhibitorCreative)))
+            {
+                incomplete.Add(smallMoleculeInhibitorCreative.ToString());
+            }
+
+            foreach (var inhibition in Enum.GetValues(typeof(Inhibition)))
+            {
+                incomplete.Add(inhibition.ToString().Split('_')[0]);
+            }
+
+            incomplete.AddRange(
+                MultiplyStem(Enum.GetValues(typeof(SmallMoleculeInhibitorCreative)).Length, typeof(SmallMoleculeInhibitorStem)));
+
+            int back = 0;
+
+            for (int i = 0; i < incomplete.Count;  i++)
+            {
+                if (i < Enum.GetValues(typeof(MonoclonalAntibodyCreative)).Length)
+                {
+                    cancerInhibitors.Add("");
+                }
+                else if ((back + 1) % Enum.GetValues(typeof(MonoclonalAntibodyCreative)).Length != 0)
+                {
+                    if (back < Enum.GetValues(typeof(MonoclonalAntibodyCreative)).Length) { }
+                    else { back = 0; }
+                }
+                cancerInhibitors[back] += incomplete[i];
+                back++;
+            }
+
+            return cancerInhibitors;
+        }
+
+        private List<string> MultiplyStem(int creativeCount, Type type)
+        {
+            List<string> stems = new List<string>();
+            for (int i = 0; i < creativeCount; i++)
+            {
+                foreach (var monoclonalAntibodySubStem in Enum.GetValues(type))
+                {
+                    stems.Add(monoclonalAntibodySubStem.ToString());
+                }
+            }
+            return stems;
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/Enum/Cancer.cs
@@ -0,0 +1,45 @@
+﻿public enum MonoclonalAntibodyStem
+{
+    mab
+}
+
+public enum MonoclonalAntibodySubStem
+{
+      xi_chimeric_human_mouse
+    , zu_humanized_mouse_drugs
+    , mu_fully_human
+}
+
+public enum MonoclonalAntibodyCreative
+{
+      ri
+    , beva
+    , ipi
+}
+
+public enum MonoclonalAntibodyTarget
+{
+      tumor
+    , circulatory_system
+    , li_immune_system
+}
+
+public enum SmallMoleculeInhibitorCreative
+{
+      ima
+    , borte
+    , seli
+}
+
+public enum Inhibition
+{
+      tin_tyrosine_kinase
+    , zom_proteasome
+    , cicl_clyclin_dependent_kinase
+}
+
+public enum SmallMoleculeInhibitorStem
+{
+    ib
+}
+
